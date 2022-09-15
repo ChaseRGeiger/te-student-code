c792eeeb519665c42bb25ce18d630abd33970d7f
Chase Geiger <chaserg1016@gmail.com>
2022-09-15 09:07:59 -0400

    commiting to pull

--- module-1/03_Logical_Branching/exercise/src/main/java/com/techelevator/Exercise04_HotelReservation.java
+++ module-1/03_Logical_Branching/exercise/src/main/java/com/techelevator/Exercise04_HotelReservation.java
@@ -24,7 +24,15 @@ public class Exercise04_HotelReservation {
     calculateStayTotal(3) ➔ 269.97
      */
     public double calculateStayTotal(int numberOfNights) {
-        return 0;
+
+        if(numberOfNights < 3)
+        {
+            return numberOfNights * DAILY_RATE;
+        }
+        else
+        {
+            return numberOfNights * DISCOUNT_RATE;
+        }
     }
 
     /*
@@ -42,7 +50,14 @@ public class Exercise04_HotelReservation {
     calculateStayTotal(3, 2) ➔ 289.97
      */
     public double calculateStayTotal(int numOfTotalNights, int numOfWeekendNights) {
-        return 0;
+       if(numOfTotalNights>=3){
+           double weeknightCost = (numOfTotalNights - numOfWeekendNights) * DISCOUNT_RATE;
+           return weeknightCost + (numOfWeekendNights * DAILY_RATE);
+       }
+       else{
+           return numOfTotalNights * DAILY_RATE;
+       }
+
     }
 
     /*
@@ -60,6 +75,17 @@ public class Exercise04_HotelReservation {
     calculateStayTotal(3, 1, true) ➔ 269.97
      */
     public double calculateStayTotal(int numOfTotalNights, int numOfWeekendNights, boolean isRewardsMember) {
-        return 0;
+        if(isRewardsMember){
+            return numOfTotalNights * DISCOUNT_RATE;
+        }
+        else if(numOfTotalNights>=3){
+            double weeknightCost = (numOfTotalNights - numOfWeekendNights) * DISCOUNT_RATE;
+            return weeknightCost + (numOfWeekendNights * DAILY_RATE);
+        }
+        else{
+            return numOfTotalNights * DAILY_RATE;
+        }
+
+
     }
 }
