104002047645af30afdbb5994269082f09f6a653
Chase Geiger <chaserg1016@gmail.com>
2022-10-12 12:50:59 -0400

    commiting to pull

--- module-2/03_Joins/exercise/Exercises/06_genres_wizard_of_oz.sql
+++ module-2/03_Joins/exercise/Exercises/06_genres_wizard_of_oz.sql
@@ -1,3 +1,10 @@
 -- 6. The genres of "The Wizard of Oz" sorted in alphabetical order (A-Z).
 -- (3 rows)
+SELECT genre_name
+FROM genre
+JOIN movie_genre ON genre.genre_id = movie_genre.genre_id
+JOIN movie ON movie_genre.movie_id = movie.movie_id
+WHERE title LIKE 'The Wizard of OZ'
+ORDER BY genre_name;
+
 
