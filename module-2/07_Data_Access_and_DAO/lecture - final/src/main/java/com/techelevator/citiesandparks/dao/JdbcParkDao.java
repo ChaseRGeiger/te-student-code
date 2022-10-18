a1da40cd81cd73928e4ddb4dfdd9fe1db3be6632
Brian Lauvray <brian@techelevator.com>
2022-10-18 13:13:02 -0400

    lecture code

--- module-2/07_Data_Access_and_DAO/lecture - final/src/main/java/com/techelevator/citiesandparks/dao/JdbcParkDao.java
+++ module-2/07_Data_Access_and_DAO/lecture - final/src/main/java/com/techelevator/citiesandparks/dao/JdbcParkDao.java
@@ -18,40 +18,101 @@ public class JdbcParkDao implements ParkDao {
 
     @Override
     public Park getPark(int parkId) {
-        return new Park();
+        Park park = null;
+        String sql = "SELECT park_id, park_name, date_established, area, has_camping FROM park WHERE park_id = ?";
+
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql, parkId);
+
+        if (results.next() ) {
+            park = mapRowToPark(results);
+        }
+
+        return park;
     }
 
     @Override
     public List<Park> getParksByState(String stateAbbreviation) {
-        return new ArrayList<Park>();
+        List<Park> parks = new ArrayList<Park>();
+        String sql = "SELECT park.park_id, park_name, date_established, area, has_camping FROM park " +
+                "JOIN park_state ON park.park_id = park_state.park_id " +
+                "WHERE park_state.state_abbreviation = ?";
+
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql, stateAbbreviation);
+
+        while (results.next()) {
+            parks.add( mapRowToPark(results) );
+        }
+
+        return parks;
     }
 
     @Override
     public Park createPark(Park park) {
-        return new Park();
+        String sql = "INSERT INTO park (park_name, date_established, area, has_camping) " +
+                "VALUES (?, ?, ?, ?) RETURNING park_id";
+        int parkId = jdbcTemplate.queryForObject(sql, Integer.class, park.getParkName(), park.getDateEstablished(),
+                park.getArea(), park.getHasCamping() );
+        park.setParkId(parkId);
+        return park;
     }
 
     @Override
     public void updatePark(Park park) {
-
+        String sql = "UPDATE park " +
+                " SET park_name=?, date_established=?, area=?, has_camping=?" +
+                " WHERE park_id=?";
+        jdbcTemplate.update(sql, park.getParkName(), park.getDateEstablished(), park.getArea(),
+                park.getHasCamping(), park.getParkId());
     }
 
     @Override
     public void deletePark(int parkId) {
-
+        // Must remove the park from all states before we can delete the park
+        List<String> statesTheParkIsIn = getStateAbbreviationForPark(parkId);
+        for (String stateAbbreviation : statesTheParkIsIn) {
+            removeParkFromState(parkId, stateAbbreviation);
+        }
+        // Once it has been removed, delete the park
+        String sql = "DELETE FROM park WHERE park_id = ?";
+        jdbcTemplate.update(sql, parkId);
     }
 
     @Override
     public void addParkToState(int parkId, String stateAbbreviation) {
-
+        String sql = "INSERT INTO park_state (park_id, state_abbreviation) VALUES (?,?)";
+        jdbcTemplate.update(sql, parkId, stateAbbreviation);
     }
 
     @Override
     public void removeParkFromState(int parkId, String stateAbbreviation) {
+        String sql = "DELETE FROM park_state WHERE park_id = ? AND state_abbreviation = ?";
+        jdbcTemplate.update(sql, parkId, stateAbbreviation);
+    }
 
+    private List<String> getStateAbbreviationForPark(int parkId) {
+        List<String> states = new ArrayList<String>();
+        String sql = "SELECT state_abbreviation " +
+                "FROM park " +
+                "JOIN park_state ON park.park_id = park_state.park_id " +
+                "WHERE park.park_id = ?";
+        SqlRowSet results = jdbcTemplate.queryForRowSet( sql, parkId );
+        while (results.next()) {
+            states.add( results.getString("state_abbreviation") );
+        }
+        return states;
     }
 
     private Park mapRowToPark(SqlRowSet rowSet) {
-        return new Park();
+        Park park = new Park();
+
+        park.setParkId( rowSet.getInt("park_id") );
+        park.setParkName( rowSet.getString("park_name") );
+        if (rowSet.getDate("date_established") != null) {
+            park.setDateEstablished(rowSet.getDate("date_established").toLocalDate());
+        }
+        park.setArea( rowSet.getDouble("area") );
+        park.setHasCamping( rowSet.getBoolean("has_camping") );
+
+        return park;
     }
 }
