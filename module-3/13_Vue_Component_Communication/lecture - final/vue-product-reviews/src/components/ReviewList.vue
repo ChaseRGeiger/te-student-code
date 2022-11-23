e1d046960f763157ab91c51241aa22f37e35d0d2
Brian Lauvray <brian@techelevator.com>
2022-11-23 12:13:03 -0500

    lecture code

--- module-3/13_Vue_Component_Communication/lecture - final/vue-product-reviews/src/components/ReviewList.vue
+++ module-3/13_Vue_Component_Communication/lecture - final/vue-product-reviews/src/components/ReviewList.vue
@@ -1,15 +1,32 @@
 <template>
-  
+  <div class="reviews">
+        <div v-if="filteredReviews.length === 0">
+            <p>No {{ $store.state.filter }} Star Reviews Found</p>
+        </div>
+      <ReviewDisplay 
+        v-for="review in filteredReviews" 
+        v-bind:key="review.id"
+        v-bind:review="review" />
+  </div>
 </template>
 
 <script>
+import ReviewDisplay from './ReviewDisplay'
+
 export default {
     name: 'review-list',
     components: {
-
+        ReviewDisplay
     },
     computed: {
-        
+        filteredReviews() {
+            const reviews = this.$store.state.reviews;
+            const currentFilter = this.$store.state.filter;
+
+            return reviews.filter( review => {
+                return  currentFilter === 0 ? true : currentFilter === review.rating;
+            });
+        },
     }
 }
 </script>
